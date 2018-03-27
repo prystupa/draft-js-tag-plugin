34f906e1b6d32330a11998f655706b9a978a9e87
eprystupa <eugene.prystupa@gmail.com>
2018-03-27 10:56:47 -0400

    - out-of-the-box mentions plugin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Editor from "draft-js-plugins-editor";
 import {EditorState} from "draft-js";
+import createMentionPlugin, {defaultSuggestionsFilter} from "draft-js-mention-plugin";
 
 import "bootstrap/dist/css/bootstrap.css";
+import "draft-js-mention-plugin/lib/plugin.css";
+
+import mentions from "./mentions";
 import "./App.css";
 
 class App extends React.Component {
@@ -11,22 +15,36 @@ class App extends React.Component {
         super(props);
 
         this.state = {
-            editor: EditorState.createEmpty()
+            editor: EditorState.createEmpty(),
+            suggestions: mentions
         };
+
+        this.mentionPlugin = createMentionPlugin();
     }
 
     onChange = (editor) => {
         this.setState({editor});
     };
 
+    onSearchChange = ({value}) => {
+        this.setState({suggestions: defaultSuggestionsFilter(value, mentions)});
+    };
+
     render() {
+        const {MentionSuggestions} = this.mentionPlugin;
+        const plugins = [this.mentionPlugin];
+
         return (
             <div className="w-100 h-100 d-flex align-items-center">
                 <div className="border w-50 mx-auto h-50 px-2"
                      style={{overflow: 'auto'}}
                      onClick={() => this.editor.focus()}>
                     <Editor ref={ref => this.editor = ref} editorState={this.state.editor}
-                            onChange={this.onChange}/>
+                            onChange={this.onChange}
+                            plugins={plugins}/>
+
+                    <MentionSuggestions suggestions={this.state.suggestions}
+                                        onSearchChange={this.onSearchChange}/>
                 </div>
             </div>
         );
