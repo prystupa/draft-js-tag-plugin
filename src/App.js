7d66d0bc70ab3796a72c0c49820921e78ef4047f
eprystupa <eugene.prystupa@gmail.com>
2018-03-27 09:19:11 -0400

    - barebone draftjs plugin editor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Editor from "draft-js-plugins-editor";
+import {EditorState} from "draft-js";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import "./App.css";
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            editor: EditorState.createEmpty()
+        };
+    }
+
+    onChange = (editor) => {
+        this.setState({editor});
+    };
+
+    render() {
+        return (
+            <div>
+                <Editor editorState={this.state.editor}
+                        onChange={this.onChange}/>
+            </div>
+        );
+    }
 }
 
 export default App;
